94b3086cd935515c5b4210b55da2b0fe129541ef
hsinlu <hsinlu@live.com>
2015-07-02 23:42:53 +0800

    article hub page layout and style

--- resources/views/articles/hub.blade.php
+++ resources/views/articles/hub.blade.php
@@ -2,9 +2,8 @@
 
 @section('content')
 <div class="hub">
-<div class="row">
-    <div class="col-md-4">
-        <div class="list-group hub-list">
+    <div class="hub-list">
+        <div class="list-group">
             @foreach($articles as $item)
                 <a href="{{ url('articles/hub/' . $item->slug) . '?page=' . $articles->currentPage() }}" class="list-group-item {{ $item->slug == $article->slug ? 'active' : '' }}">
                     {{ $item->title }}
@@ -13,25 +12,22 @@
                     </p>
                 </a>
             @endforeach
-        </div>
-        {!! $articles->render() !!}
-    </div>
-    <div class="col-md-8">
-        <div class="hub-content">
-            <div class="action-bar">
-                <a class="btn btn-default" href="{{ url('articles/edit/' . $article->slug) }}">
-                    <i class="fa fa-pencil"></i>&nbsp;{{ trans('strings.edit') }}
-                </a>
-                <a class="btn btn-danger" href="{{ url('articles/delete/' . $article->slug) }}">
-                    <i class="fa fa-trash-o"></i>&nbsp;{{ trans('strings.delete') }}
-                </a>
-            </div>
-            <div class="panel-body">
-                <h2>{{ $article->title }}</h2>
-                {!! $article->body !!}
-            </div>
+            {!! $articles->render() !!}
         </div>
     </div>
-</div>
+    <div class="hub-content">
+        <div class="action-bar">
+            <a class="btn btn-default" href="{{ url('articles/edit/' . $article->slug) }}">
+                <i class="fa fa-pencil"></i>&nbsp;{{ trans('strings.edit') }}
+            </a>
+            <a class="btn btn-danger" href="{{ url('articles/delete/' . $article->slug) }}">
+                <i class="fa fa-trash-o"></i>&nbsp;{{ trans('strings.delete') }}
+            </a>
+        </div>
+        <div class="panel-body">
+            <h2>{{ $article->title }}</h2>
+            {!! $article->body !!}
+        </div>
+    </div>
 </div>
 @endsection
