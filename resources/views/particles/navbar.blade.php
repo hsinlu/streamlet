ff6ab47d146641fd7f6a6f9d94de127e95118361
hsinlu <hsinlu@live.com>
2015-10-16 15:37:59 +0800

    调整首页的样式

--- resources/views/particles/navbar.blade.php
+++ resources/views/particles/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar navbar-default navbar-fixed-top toggle-visibility-navbar" role="banner">
-    <div class="container">
+    <div class="container-fulid">
         <div class="navbar-header">
             <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
                 <span class="sr-only">Toggle navigation</span>
@@ -13,23 +13,27 @@
             </a>
         </div>
         <nav class="collapse navbar-collapse" role="navigation">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a class="text-uppercase" href="{{ url('/') }}">{{ trans('strings.home') }}</a>
+                </li>
+                <li>
+                    <a class="text-uppercase" href="{{ url('articles') }}">{{ trans('strings.articles') }}</a>
+                </li>
+                <li>
+                    <a class="text-uppercase" href="{{ url('knots') }}">Knots</a>
+                </li>
+                <li>
+                    <a class="text-uppercase" href="{{ url('projects') }}">{{ trans('strings.projects') }}</a>
+                </li>
+            </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li>
-                    <a href="{{ url('/') }}">{{ trans('strings.home') }}</a>
-                </li>
-                <li>
-                    <a href="{{ url('articles') }}">{{ trans('strings.articles') }}</a>
-                </li>
-                <li>
-                    <a href="{{ url('knots') }}">Knots</a>
-                </li>
-                <li>
-                    <a href="{{ url('projects') }}">{{ trans('strings.projects') }}</a>
+                    <form action="{{ url('search') }}" class="form-inline">
+                        <input name="words" type="search" placeholder="{{ trans('strings.tips.input_search_words') }}" class="form-control search">
+                    </form>
                 </li>
                 @if(Auth::check())
-                    <li>
-                        <a href="{{ url('admin/articles/hub') }}">Hub</a>
-                    </li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                            aria-haspopup="true" aria-expanded="false">
@@ -38,17 +42,14 @@
                             <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu">
+                            <li><a href="/admin/articles/create">{{ trans('strings.new_article') }}</a></li>
+                            <li><a href="{{ url('admin/articles/hub') }}">{{ trans('strings.hub') }}</a></li>
                             <li><a href="{{ url('admin/settings/profile') }}">{{ trans('strings.setting') }}</a></li>
                             <li role="separator" class="divider"></li>
                             <li><a href="{{ url('auth/logout') }}">{{ trans('strings.logout') }}</a></li>
                         </ul>
                     </li>
                 @endif
-                <li>
-                    <form action="{{ url('search') }}" class="form-inline">
-                        <input name="words" type="search" placeholder="{{ trans('strings.tips.input_search_words') }}" class="form-control search">
-                    </form>
-                </li>
             </ul>
         </nav>
     </div>
